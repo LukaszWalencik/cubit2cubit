829e57dcd6a487ce080edf57d87826a93f383e03
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-19 19:34:29 +0200

    Add to CounterCubit ColorCubit as a argument to check what color backgroud have
    increment size depense of background color thats why we use if statement
    
    at the end we need to close our stream ( color listener)

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -1,8 +1,37 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:cubit2cubit/cubits/color/color_cubit.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  int incrementSize = 1;
+  final ColorCubit colorCubit;
+  late final StreamSubscription colorSubscription;
+  CounterCubit(
+    this.colorCubit,
+    this.colorSubscription,
+  ) : super(CounterState.initial()) {
+    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
+      if (colorState.color == Colors.red) {
+        incrementSize = 1;
+      } else if (colorState == Colors.green) {
+        incrementSize = 10;
+      } else if (colorState == Colors.blue) {
+        incrementSize = 100;
+      } else if (colorState == Colors.black) {
+        emit(state.copyWith(counter: state.counter - 100));
+        incrementSize = -100;
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    colorSubscription.cancel();
+    return super.close();
+  }
 }

3991e90009c5693bfcbae13a34779e5d39cda6ba
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-19 19:21:52 +0200

    Edit counter state and add variable counter there

--- lib/cubits/counter/counter_state.dart
+++ lib/cubits/counter/counter_state.dart
@@ -1,10 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'counter_cubit.dart';
 
-sealed class CounterState extends Equatable {
-  const CounterState();
+class CounterState extends Equatable {
+  final int counter;
+
+  const CounterState({required this.counter});
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+  @override
+  List<Object> get props => [counter];
 
   @override
-  List<Object> get props => [];
-}
+  bool get stringify => true;
 
-final class CounterInitial extends CounterState {}
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}

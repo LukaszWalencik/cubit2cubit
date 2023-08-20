e6d5666ffa5ecac268ef0ca36c1eb7d8f5a3e57f
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-20 19:53:37 +0200

    fix counter state to read colors from colorState

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -11,18 +11,15 @@ class CounterCubit extends Cubit<CounterState> {
   int incrementSize = 1;
   final ColorCubit colorCubit;
   late final StreamSubscription colorSubscription;
-  CounterCubit(
-    this.colorCubit,
-    this.colorSubscription,
-  ) : super(CounterState.initial()) {
+  CounterCubit({required this.colorCubit}) : super(CounterState.initial()) {
     colorSubscription = colorCubit.stream.listen((ColorState colorState) {
       if (colorState.color == Colors.red) {
         incrementSize = 1;
-      } else if (colorState == Colors.green) {
+      } else if (colorState.color == Colors.green) {
         incrementSize = 10;
-      } else if (colorState == Colors.blue) {
+      } else if (colorState.color == Colors.blue) {
         incrementSize = 100;
-      } else if (colorState == Colors.black) {
+      } else if (colorState.color == Colors.black) {
         emit(state.copyWith(counter: state.counter - 100));
         incrementSize = -100;
       }
